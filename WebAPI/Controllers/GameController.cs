d26bfc60c99c79f8db0e6666721be736d5a79bd2
Daniel Crha <daniel.crha@gmail.com>
2018-08-01 11:04:40 +0200

    Added swagger support

---
+++ WebAPI/Controllers/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        // GET: api/Game
+        // Get information about the game server
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST: api/Game
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Game;
+using Game.ActionGetters;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebAPI
 {
@@ -25,7 +30,23 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var serviceProvider = new ServiceCollection()
+            .AddMediatR(typeof(Resolver).GetTypeInfo().Assembly)
+            .AddScoped<IMediator, Mediator>()
+            .AddScoped<IColonistPickGetter, ColonistPickGetter>()
+            .AddScoped<IDrawGetter, DrawGetter>()
+            .AddScoped<IDiscardGetter, DiscardGetter>()
+            .AddScoped<IPowerGetter, PowerGetter>()
+            .AddScoped<IBuildGetter, BuildGetter>()
+            .AddScoped<IGameEndGetter, GameEndGetter>()
+            .BuildServiceProvider();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("colonizers", new Info { Title = "Colonizers game API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +62,18 @@ namespace WebAPI
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/colonizers/swagger.json", "Colonizers game API");
+                c.RoutePrefix = string.Empty; // set localhost as the swagger endpoint
+            });
+
             app.UseMvc();
         }
     }
